8cebe9cfe76e925b9ddb1009d58cbd7a46f7f1a5
aditya <es1221211@iitd.ac.in>
2024-06-23 22:35:19 +0530

    final changes done

--- stats.go
+++ stats.go
@@ -3,6 +3,7 @@ package main
 import (
 	//"github.com/go-git/go-git/v5"
 	//"gopkg.in/src-d/go-git.v4/plumbing"
+	"sort"
 	"time"
 
 	"gopkg.in/src-d/go-git.v4"
@@ -13,6 +14,9 @@ import (
 
 const outOfRange= 99999
 const daysInLastSixMonths= 183
+const weeksInLastSixMonths = 26
+
+type column []int
 
 // calcOffset determines and returns the amount of days missign to fill
 // the last row of the stats graph
@@ -119,6 +123,147 @@ func processRepositories(email string) map[int]int{
 		commits = fillCommits(email , path , commits)
 	}
 	return commits
+}
+
+// now onto printing the commits
+
+// printCommitStats prints the commit stats
+func printCommitStats(commits map[int]int){
+	keys := sortMapIntoSlice(commits)
+	cols := buildCols(keys , commits)
+	printCells(cols)
+} 
+
+// returns a slice of a map ordered
+func sortMapIntoSlice(m map[int]int) []int{
+	// order map
+	// To store the keys in slice in sorted order
+	var keys []int
+	for k := range m {
+		keys = append(keys, k)
+	}
+	sort.Ints(keys)
+
+	return keys
+}
+
+// buildCols generates a map with rows and columns ready to be printed to screen
+func buildCols(keys []int , commits map[int]int) map[int]column {
+	cols := make(map[int]column)
+	col := column{}
+
+	for _,k := range keys{
+		week := int(k/7)
+		dayinweek := k % 7
+
+		if dayinweek == 0 { // reset
+			col = column{}
+		}
+
+		col = append(col, commits[k])
+
+		if dayinweek == 6 {
+			cols[week] = col
+		}
+	}
+	return cols
+}
+
+// printCells prints the cells of the graph
+func printCells(cols map[int]column) {
+	printMonths()
+	for j := 6; j >= 0; j-- {
+		for i := weeksInLastSixMonths + 1; i >= 0; i-- {
+			if i == weeksInLastSixMonths+1 {
+				printDayCol(j)
+			}
+			if col, ok := cols[i]; ok {
+				//special case today
+				if i == 0 && j == calcOffset()-1 {
+					printCell(col[j], true)
+					continue
+				} else {
+					if len(col) > j {
+						printCell(col[j], false)
+						continue
+					}
+				}
+			}
+			printCell(0, false)
+		}
+		fmt.Printf("\n")
+	}
+}
+
+// printMonths prints the month names in the first line, determining when the month
+// changed between switching weeks
+func printMonths() {
+	week := getBeginningOfDay(time.Now()).Add(-(daysInLastSixMonths * time.Hour * 24))
+	month := week.Month()
+	fmt.Printf("         ")
+	for {
+		if week.Month() != month {
+			fmt.Printf("%s ", week.Month().String()[:3])
+			month = week.Month()
+		} else {
+			fmt.Printf("    ")
+		}
+
+		week = week.Add(7 * time.Hour * 24)
+		if week.After(time.Now()) {
+			break
+		}
+	}
+	fmt.Printf("\n")
+}
+
+// printDayCol given the day number (0 is Sunday) prints the day name,
+// alternating the rows (prints just 2,4,6)
+func printDayCol(day int) {
+	out := "     "
+	switch day {
+	case 1:
+		out = " Mon "
+	case 3:
+		out = " Wed "
+	case 5:
+		out = " Fri "
+	}
+
+	fmt.Printf(out)
+}
+
+// printCell given a cell value prints it with a different format
+// based on the value amount, and on the `today` flag.
+func printCell(val int, today bool) {
+    escape := "\033[0;37;30m"
+    switch {
+    case val > 0 && val < 5:
+        escape = "\033[1;30;47m"
+    case val >= 5 && val < 10:
+        escape = "\033[1;30;43m"
+    case val >= 10:
+        escape = "\033[1;30;42m"
+    }
+
+    if today {
+        escape = "\033[1;37;45m"
+    }
+
+    if val == 0 {
+        fmt.Printf(escape + "  - " + "\033[0m")
+        return
+    }
+
+    str := "  %d "
+    switch {
+    case val >= 10:
+        str = " %d "
+    case val >= 100:
+        str = "%d "
+    }
+
+    fmt.Printf(escape+str+"\033[0m", val)
 }
 
 // calculates and prints the stats
