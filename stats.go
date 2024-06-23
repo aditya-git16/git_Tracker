eb873b9592956c653a94ae7c3d3f3c5df6d3787f
aditya <es1221211@iitd.ac.in>
2024-06-23 12:34:44 +0530

    go mod and go-git added

--- stats.go
+++ stats.go
@@ -1,9 +1,20 @@
 package main
 
 import(
+	"github.com/go-git/go-git/v5"
+	"gopkg.in/src-d/go-git.v4/plumbing"
 
+	"fmt"
 )
 
-func stats(email string)  {
+// processRepositories given a user email , returns the commits in the last 6 months 
+func processRepositories(email string) map[int]int{
 	
 }
+
+// calculates and prints the stats
+func stats(email string)  {
+	commits := processRepositories(email)
+	printCommitStats(commits)
+}
+
