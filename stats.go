a113fcd14ea918ad0d4659804bfc63e1ab137f85
aditya <es1221211@iitd.ac.in>
2024-06-23 17:38:33 +0530

    processing commits done

--- stats.go
+++ stats.go
@@ -1,13 +1,64 @@
 package main
 
-import(
+import (
 	//"github.com/go-git/go-git/v5"
 	//"gopkg.in/src-d/go-git.v4/plumbing"
+	"time"
+
 	"gopkg.in/src-d/go-git.v4"
 	"gopkg.in/src-d/go-git.v4/plumbing/object"
 
 	"fmt"
 )
+
+const outOfRange= 99999
+const daysInLastSixMonths= 183
+
+// calcOffset determines and returns the amount of days missign to fill
+// the last row of the stats graph
+
+func calcOffset() int {
+	var offset int
+	weekday := time.Now().Weekday()
+	switch weekday{
+	case time.Sunday:
+		offset = 7
+	case time.Monday:
+		offset = 6
+	case time.Tuesday:
+		offset = 5
+	case time.Wednesday:
+		offset = 4
+	case time.Thursday:
+		offset = 3
+	case time.Friday:
+		offset = 2
+	case time.Saturday:
+		offset = 1
+	}
+	return offset
+}
+
+// getBeginningOfDay given a time. Time calculates the start time of that day
+func getBeginningOfDay(t time.Time) time.Time{
+	year , month , day := t.Date()
+	startOfDay := time.Date(year,month,day,0,0,0,0,t.Location())
+	return startOfDay
+}
+
+// countDaysSinceDate counts how many days passed since the passed 'date'
+func countDaysSinceDate(date time.Time) int{
+	days :=0
+	now := getBeginningOfDay(time.Now())
+	for date.Before(now){
+		date = date.Add(time.Hour *24)
+		days ++
+		if days > daysInLastSixMonths{
+			return outOfRange
+		}
+	}
+	return days
+}
 
 // fillCommits given a repository found in 'path' , gets the commits and
 // puts them in to the 'commits' map , returning it when completed
